257efb8611ef4e2383b7bcd873ba4dec3fec3394
zuhaib786 <suhaibulsaman@gmail.com>
2022-09-11 14:00:39 +0530

    Changes

--- code.cpp
+++ code.cpp
@@ -1,34 +1,62 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h> 
 using namespace std;
-using lli = long long int;
-using pii = pair<lli, lli>;
-const int maxN = 3e5 + 5;
-#define ff first
-#define ss second
-lli arr[maxN];
-lli dp[maxN];
-void solve()
-{   
-    int n;
-    cin>>n;
-    for(int i =0; i<n; i++)
+pair<vector<long long >, int> merge(long long * arr, int l, int r)
+{
+    
+    if(l == r)
     {
-        cin>>arr[i];
+        return {vector<long long >(1,arr[l]), 0};        
     }
-    for(int i = 0; i < n; i++)
+    int mid = (l + r)/2;
+    pair<vector<long long >, int>p1 = merge(arr, l, mid);
+    pair<vector<long long >, int> p2 = merge(arr, mid + 1, r);
+    
+    int L = 0;
+    int R = 0;
+    int inversion_cnt = p1.second + p2.second;
+    vector<long long >save(p1.first.size() + p2.first.size(), 0);
+    while(L<p1.first.size() && R < p2.first.size())
     {
-        
+        if(p1.first[L] > p2.first[R])
+        {
+            auto idx = p1.first.end() - (p1.first.begin() + L, p1.first.end(), 2 * p2.first[R]);
+            inversion_cnt += (p1.first.size() - L);
+            save[L + R] = p2.first[R];
+            R++;
+        }
+        else
+        {
+            save[L + R] = p1.first[L];
+            L++;
+        }
     }
+    while(L<p1.first.size())
+    {
+        save[L + R] = p1.first[L];
+        L++;
+    }
+    while(R < p2.first.size())
+    {
+        save[L + R] = p2.first[R];
+        R++;
+    }
+    // cout<<l<<" "<<mid <<" "<<r<<" "<<inversion_cnt <<'\n';
+    return {save, inversion_cnt};
+}
+long long getInversions(long long *arr, int n){
+    // Write your code here.
+    auto p = merge(arr, 0, n - 1);
+    return p.second;
+    
 }
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;
-    cin>>t;
-    while(t--)
+    int n;
+    cin>>n;
+    long long  arr[n];
+    for(int i = 0; i<n; i++)
     {
-        solve();
+        cin>>arr[i];
     }
-    
+    cout<<getInversions(arr, n)<<'\n';
 }
